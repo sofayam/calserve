aca34b2f5666caa7efb99a475ad8cd74a5666e39
Mark on RPM17 <mark.andrew@gmail.com>
2025-07-03 14:27:48 +0000

    clean now

--- junk/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script builds the Docker image for the Calibre server.
-
-echo "Building Calibre server Docker image..."
-docker build -t calibre-server:latest .
-echo "Build complete."
