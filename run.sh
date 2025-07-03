ab56149b84bf3ea5a47b1f956c415e69a1ec3414
Mark on RPM17 <mark.andrew@gmail.com>
2025-07-03 14:24:21 +0000

    this seems to work

--- run.sh
+++ run.sh
@@ -1,13 +1,11 @@
-docker run -d \
-  --name=calibre-web \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ=Etc/UTC \
-  -e DOCKER_MODS=linuxserver/mods:universal-calibre `#optional` \
-  -e OAUTHLIB_RELAX_TOKEN_SCOPE=1 `#optional` \
-  -p 8015:8015 \
-  -v /volume1/docker/calserve/data:/config \
-  -v /var/services/homes/mark/Drive/monsterbookcollection:/books \
-  --restart unless-stopped \
-  lscr.io/linuxserver/calibre-web:latest
-
+docker run -d --name=calibre-web \
+-p 8083:8083 \
+-e PUID=1027 \
+-e PGID=100 \
+-e TZ=Europe/Bucharest \
+-e DOCKER_MODS=linuxserver/mods:universal-calibre \
+-e OAUTHLIB_RELAX_TOKEN_SCOPE=1 \
+-v /volume1/docker/calserve/data:/config \
+-v /var/services/homes/mark/Drive/monsterbookcollection:/books \
+--restart always \
+ghcr.io/linuxserver/calibre-web
