2f02f8130c701a4dc5d8347391014768c42c6f86
Mark on RPM17 <mark.andrew@gmail.com>
2025-07-31 16:04:51 +0000

    working run script

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 docker run -d --name=calibre-web \
--p 8083:8083 \
+-p 8015:8083 \
 -e PUID=1027 \
 -e PGID=100 \
--e TZ=Europe/Bucharest \
+-e TZ=Europe/Lisbon \
 -e DOCKER_MODS=linuxserver/mods:universal-calibre \
 -e OAUTHLIB_RELAX_TOKEN_SCOPE=1 \
 -v /volume1/docker/calserve/data:/config \
--v /var/services/homes/mark/Drive/monsterbookcollection:/books \
+-v /var/services/homes/mark/Drive/monsterbookcollection:/books:ro \
 --restart always \
 ghcr.io/linuxserver/calibre-web
